d509369bfa9971806ca6946c67d7ac62190fae42
Hanh Huyen My Nguyen <mynguyen15@Mac-von-My.local>
2022-06-25 19:44:42 +0200

    add LoadDataService

--- client/js/index.js
+++ client/js/index.js
@@ -1,7 +1,16 @@
-function loadDataFromServer(){
-  // Anmerkung: Hier ist fetch(url, {mode: "cors"}) zu nutzen.
-}
-
-function loadDataFromFile(){
-
+class LoadDataService {
+  async loadDataFromServer(){
+    const modules = await fetch("http://localhost:3000/modules", { mode: "cors" })
+                          .then((response) => response.json()) 
+                          .then((modules) => {
+                            return modules;
+                          });
+    const moduleGroups = await fetch("http://localhost:3000/module_groups", { mode: "cors" })
+                              .then((response) => response.json()) 
+                              .then((groups) => {
+                                return groups;
+                              });
+    const data = {modules: modules, moduleGroups: moduleGroups};
+    return data;
+  }
 }
