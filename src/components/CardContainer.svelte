8794e569261d4fbb38ce634900b14dc2afad2db7
Hanh Huyen My Nguyen <mynguyen15@Mac-von-My.local>
2022-07-15 00:28:24 +0200

    implement sorting by name and by index

--- src/components/CardContainer.svelte
+++ src/components/CardContainer.svelte
@@ -8,20 +8,53 @@
 	let selectedSortingCriterion;
 	selectedGrouping.subscribe(value => selectedGroupingCriterion = value)
 	selectedSorting.subscribe(value => selectedSortingCriterion = value)
-    
+        
+    //allStates, allPlacesData, sortedDataGroupedByState must be assigned a value inside a function such as following, which has "data" as param, otherwise not updated
     let allStates = [];
-    let allPlacesData = []
-    //allStates and allPlacesData must be assigned a value inside a function such as following, which has "data" as param, otherwise not updated
-    const getAllPlacesData = (data) => {
+    const getAllStates = (data) => {
         allStates = Object.keys(data); 
+    }
+
+    let allPlacesData = [];
+    const getAllPlacesData = (data) => {
         for(const state in data){
             // for(const place of data[state]){
             //     allPlacesData = [...allPlacesData, {placeName: Object.keys(place)[0], state: state, index: Object.values(place)[0]}] //Don't use only allPlacesData.push() because in Svelte, DOM only updates on variable assignments
             // }
             allPlacesData = allPlacesData.concat(data[state])
         }
+        allPlacesData = sortCards(allPlacesData);
     }
-    $: data && getAllPlacesData(data) // If data is successfully fetched, getAllPlacesData will be executed. 
+    
+    const sortDataGroupedByState = (data) => {
+        for(const state in data){
+            data[state] = sortCards(data[state]);
+        }
+    }
+    
+    function sortCards(data) {
+        let sortedArr = [];
+        if (selectedSortingCriterion === 'name') {
+            sortedArr = data.sort((a, b) => {
+                let nameA = Object.keys(a)[0];
+                let nameB = Object.keys(b)[0];
+                return nameA > nameB ? 1 : -1;
+            })
+        }
+        if (selectedSortingCriterion === 'index') {
+            sortedArr = data.sort((a, b) => {
+                let indexA = Object.values(a)[0]; 
+                let indexB = Object.values(b)[0]; 
+                return indexA > indexB ? -1 : 1;
+            })
+        }
+        return sortedArr;
+    }
+
+    // If data is successfully fetched, these functions will be executed. 
+    $: data && getAllStates(data) 
+    $: data && getAllPlacesData(data) 
+    $: data && sortDataGroupedByState(data) 
 </script>
 
 {#if selectedGroupingCriterion === "place"}
--- src/stores.js
+++ src/stores.js
@@ -2,3 +2,5 @@ import { writable } from "svelte/store";
 
 export const selectedGrouping = writable("place");
 export const selectedSorting = writable("name");
+
+
