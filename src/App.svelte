43ce4d19bbcc6c64815adc4c32acb963b31f0a2d
Hanh Huyen My Nguyen <mynguyen15@Mac-von-My.local>
2022-07-14 19:13:44 +0200

    create project and main component structure

---
+++ src/App.svelte
@@ -0,0 +1,26 @@
+<script>
+	import { onMount } from "svelte";
+	import { Router, Route } from "svelte-navigator";
+	import Navbar from "./components/Navbar.svelte"
+	import CardContainer from "./components/CardContainer.svelte"
+	import Settings from "./components/Settings.svelte";
+	
+	
+	
+</script>
+
+
+<style>
+	main {
+		text-align: left;
+		padding: 1em;
+		width: 90%;
+		margin: 100px auto;
+	}
+
+	/* @media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	} */
+</style>
