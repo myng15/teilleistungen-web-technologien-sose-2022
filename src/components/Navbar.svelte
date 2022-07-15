e67ad278e1a64adb73e3a7dac2b129ddef22050b
Hanh Huyen My Nguyen <mynguyen15@Mac-von-My.local>
2022-07-15 17:06:02 +0200

    fix Navbar styling, add activestyling class to current active link

--- src/components/Navbar.svelte
+++ src/components/Navbar.svelte
@@ -1,18 +1,28 @@
 <script>
     import { Router, Link } from "svelte-navigator";
+    let current = 0;
 </script>
 
 <Router>
     <nav class="navbar navbar-expand-sm fixed-top navbar-dark bg-dark">
         <div class="container-fluid">
-            <Link class="navbar-brand" to="overview">Waldbrand-Vorhersage</Link>
+            <Link class="navbar-brand m-3" to="overview"><h3>Waldbrand-Vorhersage</h3></Link>
             <div class="collapse navbar-collapse" id="navbarNav"> 
                 <ul class="navbar-nav">
                     <li class="nav-item">
-                        <Link class="nav-link active" to="overview">Übersicht</Link>
+                        <Link class="nav-link" to="overview">
+                            <span
+                            on:click="{() => current = 1}"
+                            class:active="{current === 1}">
+                            Übersicht
+                            </span>
+                        </Link>
                     </li>
                     <li class="nav-item">
-                        <Link class="nav-link" to="settings">Einstellungen</Link> </li>
+                        <Link class="nav-link" to="settings"> <span
+                            on:click="{() => current = 2}"
+                            class:active="{current === 2}">Einstellungen</span></Link>
+                    </li>
                 </ul> 
             </div>
         </div>
@@ -20,5 +30,12 @@
 </Router>
 
 <style>
-    
+nav {
+    height: 100px;
+}
+
+.active {
+	color: #fda50f;
+	font-weight: bold;
+}
 </style>
